c0c7197b6084dbbe15ac47630b81c9223ffc4150
rlaaudwns05-dotcom <rlaaudwns05@gmail.com>
2026-05-28 13:14:56 +0900

    롤백
    Replaced the message separator with a simpler '/' character.

--- Hello__/src/Protocol.java
+++ Hello__/src/Protocol.java
@@ -5,6 +5,6 @@ public class Protocol {
     // 귓속말 메시지 타입 (특정 1명에게만 전송)
     public static final String WHISPER = "WHISPER";
     
-    // ⭐ 메시지 구분자 (기존 "/" 대신 본문 슬래시 충돌을 방지하기 위해 안전한 기호 조합 사용)
-    public static final String SEPARATOR = "◀▶";
+    // 메시지 구분자
+    public static final String SEPARATOR = "/";
 }
